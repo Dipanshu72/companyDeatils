c7cdae5b08f4a717f28a2b853c20db479c4e5fbb
Dipanshu72 <dipanshudiwakar@gmail.com>
2025-01-23 19:27:29 +0530

    initit changes

--- src/pages/api/companies.js
+++ src/pages/api/companies.js
@@ -13,13 +13,5 @@ export default async function handler(req, res) {
       res.status(500).json({ message: 'Error fetching data' });
     }
 
-    try {
-      const client = await connectToDatabase();
-      
-      const result = await client.query('SELECT * FROM your_table WHERE your_column = $1', [query.value]);
-      res.status(200).json(result?.rows);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      res.status(500).json({ message: 'Error fetching data' });
-    }
+    
   }
